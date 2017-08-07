991c2949105f000c6b37501b8da7d5110de06319
itskp <kapilgajarmal@gmail.com>
2017-08-08 01:06:19 +0530

    add,update feature

--- src/todo/controller.js
+++ src/todo/controller.js
@@ -1,19 +1,55 @@
 angular.module('todo.controller', [])
-    .value('serverUrl','http://localhost:8000/')
-    .factory('todoFactory',function($http,serverUrl){
+    .value('serverUrl', 'http://localhost:8000/')
+    .factory('todoFactory', function ($http, serverUrl) {
         return {
-            get: function(requestBody){
-                return $http.post(serverUrl+'get/',requestBody)
+            get: function (requestBody) {
+                return $http.post(serverUrl + 'get/', requestBody)
             },
-            create: function(requestBody){
-                return $http.post(serverUrl+'create/',requestBody)
+            create: function (requestBody) {
+                return $http.post(serverUrl + 'create/', requestBody)
+            },
+            updateStatus: function (requestBody) {
+                return $http.post(serverUrl + 'updateStatus/', requestBody)
+            },
+            markAll: function (requestBody) {
+                return $http.post(serverUrl + 'markAll/', requestBody)
             }
         }
     })
-    .controller('todoCtrl',function(todoFactory){
+    .controller('todoCtrl', function (todoFactory) {
         var vm = this
+        vm.todo = null
+        vm.marked = 0
 
-        todoFactory.get({}).then(function(response){
-            vm.todo = response.data
+        todoFactory.get({}).then(function (response) {
+            vm.todoList = response.data
         })
+
+        vm.createTodo = function () {
+            todoFactory.create({
+                name: vm.todo
+            }).then(function (response) {
+                vm.todoList.push(response.data)
+                vm.todo = null
+            })
+        }
+
+        vm.updateStatus = function (todo, index) {
+            todoFactory.updateStatus({
+                todoid: todo._id,
+                status: vm.todoList[index].status
+            }).then(function (response) {
+                vm.todoList[index] = response.data
+            })
+        }
+
+        vm.toggleStatus = function () {
+            vm.marked = !vm.marked
+            todoFactory.markAll({
+                status: vm.marked
+            }).then(function (response) {
+                vm.todoList = response.data
+            })
+
+        }
     })
